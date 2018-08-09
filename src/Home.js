ae15eee9b34a740879d9ca1bb3b95f0e0eb399a4
Nicole Chow <nicolechowt@gmail.com>
2018-08-08 18:00:44 -0700

    basic layout of site

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Today from './Today';
+
+class Home extends Component {
+	render() {
+		return(
+			<div>
+	          <Today/>
+			</div>
+		)
+	}
+}
+
+export default Home;
